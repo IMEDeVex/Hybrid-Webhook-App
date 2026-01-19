bfd324a8959ef5a22bd84e0ef4294c8378f13a92
Emnai <emnai.dev@gmail.com>
2026-01-19 15:14:54 -0500

    Add server keys and version to config

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export const SERVER_PRIVATE_KEY = `
+-----BEGIN PGP PRIVATE KEY BLOCK-----
+<your private key>
+-----END PGP PRIVATE KEY BLOCK-----
+`;
+
+export const SERVER_PUBLIC_KEY = `
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+<your public key>
+-----END PGP PUBLIC KEY BLOCK-----
+`;
+
+export const SERVER_VERSION = "1.0.0";
